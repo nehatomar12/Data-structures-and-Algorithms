45e909a50c457e647a00ac5d85b5817e339af6e2
Neha.Tomar <neha.tomar@M305393FCHTDF.community.veritas.com>
2020-12-25 22:31:12 +0530

    Check BST or not

---
+++ Tree/check_BST.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+
+# return True if the given tree is a BST, else return False
+prev = None
+def isbst_rec(root):
+    global prev
+    if root is None:
+        return True
+    if not isbst_rec(root.left):
+        return False
+    if prev and prev.data >= root.data:
+        return False
+    prev = root
+    return isbst_rec(root.right)
+
+def isBST(root):
+    if isbst_rec(root):
+        return True
+    return False
+
+
+
+
+if __name__ == '__main__':
+    root = Node(3)
+    root.left = Node(2)
+    root.right = Node(5)
+    root.left.left = Node(1)
+    root.right.left = Node(4)
+    #root.right.left.left = Node(40)
+
+    if (isBST(root)):
+        print("Is BST")
+    else:
+        print("Not a BST")
