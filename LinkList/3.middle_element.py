31852646f30eaeeb10f52f3b6e970d05d5cc7bad
Neha.Tomar <neha.tomar@M305393FCHTDF.community.veritas.com>
2020-12-28 21:05:41 +0530

    middle element in a linked list

---
+++ LinkList/3.middle_element.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LL:
+    def __init__(self):
+        self.head = None
+
+    def empty(self):
+        if self.head is None:
+            return True
+        return False
+
+    def addLast(self, data):
+        newNode = Node(data)
+        if self.empty():
+            self.head = newNode
+        else:
+            trav = self.head
+            while trav.next is not None:
+                trav = trav.next
+            trav.next = newNode
+
+    def listprint(self, head):
+        trav = head
+        while trav is not None:
+            print(trav.data, end=" ")
+            trav = trav.next
+        print()
+
+    def findMiddle(self):
+        if self.head is None:
+            return None
+        slow = fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        print(slow.data)
+
+list_obj = LL()
+l1 = [1, 2, 3, 4, 5, 6]
+
+for i in l1:
+    list_obj.addLast(i)
+
+list_obj.findMiddle()
